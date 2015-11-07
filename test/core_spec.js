731b7617dfc8257fc15615d35efc6a56c33caaee
Brandon Pugh <bp@brandonpugh.com>
2015-11-07 11:54:57 -0600

    Add basic vote logic

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,7 @@
 import {expect} from 'chai';
-import {List, Map} from 'immutable';
+import {List, Map, fromJS} from 'immutable';
 
-import {setEntries} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
   describe('setEntries', () => {
@@ -14,4 +14,56 @@ describe('application logic', () => {
       }));
     });
   });
+  describe('next', () => {
+    it('takes the next 2 entries under vote', () => {
+      const state = Map({
+        entries: List.of('Matrix', 'Hitch', 'Serendipity')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Matrix', 'Hitch')
+        }),
+        entries: List.of('Serendipity')
+      }));
+    });
+  });
+  const moviePair = ['Matrix', 'Hitch'];
+
+  describe('vote', () => {
+    it('creates a tally for the voted entry', () => {
+      const state = fromJS({
+        vote: {
+          pair: moviePair
+        },
+        entries: []
+      });
+      const nextState = vote(state, 'Matrix');
+      expect(nextState).to.equal(fromJS({
+        vote: {
+          pair: moviePair,
+          tally: {Matrix: 1}
+        },
+        entries: []
+      }));
+    });
+    it('adds to existing tally for voted entry', () => {
+      const state = fromJS({
+        vote: {
+          pair: moviePair,
+          tally: {Matrix: 2, Hitch: 1}
+        },
+        entries: []
+      });
+      const nextState = vote(state, 'Matrix');
+      expect(nextState).to.equal(fromJS({
+        vote: {
+          pair: moviePair,
+          tally: {Matrix: 3, Hitch: 1}
+        },
+        entries: []
+      }));
+    });
+  });
+
 });
