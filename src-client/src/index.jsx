dda1c84d9fda4fbb5e503bec26852e72ec7e2f68
Brandon Pugh <bp@brandonpugh.com>
2015-11-16 15:52:23 -0600

    Render basic voting component
    Also added basic styles

--- src-client/src/index.jsx
+++ src-client/src/index.jsx
@@ -1,1 +1,10 @@
-console.log('im alive');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Voting from './components/Voting';
+require('./style.css');
+const pair = ['Transpotting', '28 Days Later'];
+
+ReactDOM.render(
+  <Voting pair={pair} hasVoted="Transpotting"/>,
+  document.getElementById('app')
+);
